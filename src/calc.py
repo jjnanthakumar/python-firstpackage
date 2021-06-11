3610f156200eac67f88d3e4f05d5ced18ff614f1
Nanthakumar J J <46053893+jjnanthakumar@users.noreply.github.com>
2021-06-11 19:24:45 +0530

    first commit

---
+++ src/calc.py
@@ -0,0 +1,19 @@
+from functools import reduce
+class Calculator:
+    """Pass data as seperate arguments"""
+    def __init__(self, *args):
+        self.data = args
+    
+    def add(self):
+        return sum(self.data)
+
+    def sub(self):
+        return reduce(lambda a,b:a-b, self.data)
+    
+    def mul(self):
+        return reduce(lambda a,b:a*b, self.data)
+    
+    def div(self):
+        return reduce(lambda a,b:a/(b or 1), self.data)
+        
+        
